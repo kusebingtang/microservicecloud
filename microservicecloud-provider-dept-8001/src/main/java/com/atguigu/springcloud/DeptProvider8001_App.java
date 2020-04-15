94f539db4304fec47c3c6098157dd8cdc3ddb4b3
jbw <jb@98game.cn>
2020-04-15 17:52:36 +0800

    dept finish

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JiangBin
+ * @create 2020-04-15 17:40
+ */
+
+
+@SpringBootApplication
+public class DeptProvider8001_App {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DeptProvider8001_App.class, args);
+    }
+}
