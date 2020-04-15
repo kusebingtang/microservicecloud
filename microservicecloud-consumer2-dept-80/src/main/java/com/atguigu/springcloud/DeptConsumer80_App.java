afec02ed7a95ca56f43474cf2b23e2300af36084
jbw <jb@98game.cn>
2020-04-15 20:01:36 +0800

    microservicecloud-consumer

---
+++ microservicecloud-consumer2-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JiangBin
+ * @create 2020-04-15 19:54
+ */
+
+
+@SpringBootApplication
+public class DeptConsumer80_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer80_App.class, args);
+    }
+}
