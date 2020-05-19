4b25166d769e8f9957716102426d901ebe210162
jbw <jb@98game.cn>
2020-05-19 15:59:54 +0800

    SpringCloud_Ribbon配置初步,使用MICROSERVICECLOUD-DEPT访问provider

--- microservicecloud-consumer2-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer2-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author JiangBin
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
--- microservicecloud-consumer2-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer2-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.cfgbeans;
 
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ public class ConfigBean //boot -->spring   applicationContext.xml --- @Configura
 {
 
 	@Bean
+	@LoadBalanced//Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具。
 	public RestTemplate getRestTemplate()
 	{
 		return new RestTemplate();
